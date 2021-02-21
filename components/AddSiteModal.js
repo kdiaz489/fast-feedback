1726147da10df39d521573c82d9a5b01eb0464a9
Karen Diaz <kstefdiaz@gmail.com>
2021-02-21 15:42:56 -0800

    add site modal updated

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -13,10 +13,18 @@ import {
   useDisclosure,
   Button
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { createSite } from '@/lib/db';
 const AddSiteModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const { register, handleSubmit } = useForm();
   const initialRef = useRef();
+  const finalRef = useRef();
+
+  const onSubmit = (values) => {
+    console.log(values);
+    createSite(values);
+  };
 
   return (
     <>
@@ -26,23 +34,32 @@ const AddSiteModal = () => {
 
       <Modal
         initialFocusRef={initialRef}
-        // finalFocusRef={finalRef}
+        finalFocusRef={finalRef}
         isOpen={isOpen}
         onClose={onClose}
       >
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent as="form" onSubmit={handleSubmit(onSubmit)}>
           <ModalHeader fontWeight="bold">Add Site</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Name</FormLabel>
-              <Input ref={initialRef} placeholder="My Site" />
+              <Input
+                ref={initialRef}
+                placeholder="My Site"
+                name="site"
+                ref={register({ required: 'Required' })}
+              />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Link</FormLabel>
-              <Input placeholder="https://website.com" />
+              <Input
+                placeholder="https://website.com"
+                name="link"
+                ref={register({ required: 'Required' })}
+              />
             </FormControl>
           </ModalBody>
 
@@ -51,6 +68,7 @@ const AddSiteModal = () => {
               Cancel
             </Button>
             <Button
+              type="submit"
               backgroundColor="#99FFFE"
               color="#194D4C"
               fontWeight="medium"
